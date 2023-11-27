2c369d17d9e809e12cede4e6d157260dcf29947b
robertors <roberto@rssolucoesweb.com.br>
2023-11-27 19:55:47 -0300

    Google-Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import "./globals.css";
 import NextAuthSessionProvider from "@/providers/sessionProvider";
+import Analytics from "@/components/Analyticts/page";
 
 export const metadata = {
   title: "Pesquisa Educação",
@@ -13,6 +14,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <NextAuthSessionProvider>
             {children}
         </NextAuthSessionProvider>
+        <Analytics/>
       </body>
     </html>
   );

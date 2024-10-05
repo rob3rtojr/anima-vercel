95eca6c0740aa008111c928dcd846466add3f390
robertors <roberto@rssolucoesweb.com.br>
2024-10-05 11:08:14 -0300

    manual-aplicador-go

--- src/app/manual-aplicador-go/page.tsx
+++ src/app/manual-aplicador-go/page.tsx
@@ -4,7 +4,7 @@ export default function ManualAplicadorGo() {
     return (
         <div className='w-full h-screen bg-slate-50'>
             <object className='w-full h-full' data="https://rsadmweb.com.br/anima/manual-aplicador-go.pdf" type="application/pdf">
-                <p><a href="https://rsadmweb.com.br/anima/manual-aplicador-go.pdf">Clique aqui</a> para baixar o pdf</p>
+                <p><a href="https://rsadmweb.com.br/anima/manual-aplicador-go.pdf">Clique aqui</a> para baixar o pdf.</p>
             </object>
         </div>
     )

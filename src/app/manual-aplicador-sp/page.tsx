17045e0fb3faedfa22f8c806ad841e9be482dec1
robertors <roberto@rssolucoesweb.com.br>
2024-12-02 17:36:37 -0300

    MANUAL APLICADOR SP

---
+++ src/app/manual-aplicador-sp/page.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+export default function ManualAplicadorSP() {
+    return (
+        <div className='w-full h-screen bg-slate-50'>
+            <object className='w-full h-full' data="https://rsadmweb.com.br/anima/manual-aplicador-sp.pdf" type="application/pdf">
+                <p><a href="https://rsadmweb.com.br/anima/manual-aplicador-sp.pdf">Clique aqui</a> para baixar o pdf</p>
+            </object>
+        </div>
+    )
+}
